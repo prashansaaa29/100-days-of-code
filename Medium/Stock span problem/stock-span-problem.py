dc3274fb55ffeaf23f79a642d7e914a995479a16
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-06-05 14:42:00 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Stock span problem/stock-span-problem.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to calculate the span of stockâ€™s price for all n days.
+    def calculateSpan(self,a,n):
+        Arr=[1]*n
+        stack=[0]
+        for i in range(1,n):
+            while stack and a[i]>=a[stack[-1]]:
+                Arr[i]+=Arr[stack.pop()]
+            stack.append(i)
+        return Arr
+            
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nikhil Kumar Singh
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        obj = Solution()
+        ans = obj.calculateSpan(a, n);
+        for i in range(n):
+            print(ans[i],end=" ")
+        print()            
+# } Driver Code Ends
