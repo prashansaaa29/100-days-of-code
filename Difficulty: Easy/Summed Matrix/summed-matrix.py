8612effefed9ff86caa5e37cf0b602ec860260c8
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-06-24 19:09:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+    def sumMatrix(self, n, q):
+        if(q>1 and q<2*n+1):
+            if(q<=n+1):
+                return q-1
+            else:
+                return n+1-(q-(n+1))-1
+        else:
+            return 0
+        # code here 
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n, q = map(int, input().split())
+
+        ob = Solution()
+        print(ob.sumMatrix(n, q))
+
+# } Driver Code Ends
