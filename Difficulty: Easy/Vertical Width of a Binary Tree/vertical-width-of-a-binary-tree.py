d97ea79a67a101a5a5a5a0b79803b54830f862d2
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-07-05 00:58:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Vertical Width of a Binary Tree/vertical-width-of-a-binary-tree.py
@@ -0,0 +1,110 @@
+from collections import deque
+def verticalWidth(root):
+    # code here
+    level_set = set()
+    queue = deque()
+    
+    if not root:
+        return 0
+    
+    queue.append((root, 0))
+    level_set.add(0)
+    
+    while len(queue)>0:
+        
+        node, level = queue.popleft()
+        
+        if node.left is not None:
+            left_node = node.left
+            left_level = level-1
+            level_set.add(left_level)
+            queue.append((left_node, left_level))
+        
+        if node.right is not None:
+            right_node = node.right
+            right_level = level+1
+            level_set.add(right_level)
+            queue.append((right_node, right_level))
+    
+    return len(level_set)
+
+#{ 
+ # Driver Code Starts
+from collections import deque
+
+
+# Tree Node
+class Node:
+
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+
+# Function to Build Tree
+def buildTree(s):
+    #Corner Case
+    if (len(s) == 0 or s[0] == "N"):
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size + 1
+
+    # Starting from the second element
+    i = 1
+    while (size > 0 and i < len(ip)):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size - 1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if (currVal != "N"):
+
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size + 1
+        # For the right child
+        i = i + 1
+        if (i >= len(ip)):
+            break
+        currVal = ip[i]
+
+        # If the right child is not null
+        if (currVal != "N"):
+
+            # Create the right child for the current node
+            currNode.right = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.right)
+            size = size + 1
+        i = i + 1
+    return root
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(0, t):
+        s = input()
+        root = buildTree(s)
+        print(verticalWidth(root))
+
+# } Driver Code Ends
