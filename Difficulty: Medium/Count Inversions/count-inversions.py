d4a9349a8736af80d17b39c494eaf8e79676eeb3
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-08-11 23:37:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.py
@@ -0,0 +1,38 @@
+class Solution:
+    #User function Template for python3
+    
+    # arr[]: Input Array
+    # N : Size of the Array arr[]
+    #Function to count inversions in the array.
+    def inversionCount(self, arr, n):
+        # Your Code Here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        n = int(input())
+        a = list(map(int, input().strip().split()))
+        obj = Solution()
+        print(obj.inversionCount(a, n))
+
+# } Driver Code Ends
