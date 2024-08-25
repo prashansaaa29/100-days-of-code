fd1d84dd605f72b367b3e69b3458289c444a3bfd
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-08-25 23:48:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -4,20 +4,18 @@ class Solution:
     
     #Function to return max value that can be put in knapsack of capacity W.
     def knapSack(self,W, wt, val):
-        N = len(wt) 
-        DP = [[0 for i in range(W+1)] for j in range(N+1)]
-        
-        for i in range(1, N+1):
-            for w in range(1, W+1):
-                
-                j = w-wt[i-1]
-                
-                if j >= 0 :
-                    DP[i][w] = max(DP[i-1][w], (DP[i-1][j]+val[i-1]))
+        n=len(wt)
+        dp=[[0 for i in range(W+1)] for j in range(n+1)]
+        for i in range(1,n+1):
+            for w in range(1,W+1):
+                if w-wt[i-1]>=0:
+                    dp[i][w]=max(dp[i-1][w],dp[i-1][w-wt[i-1]]+val[i-1])
                 else:
-                    DP[i][w] = DP[i-1][w]  
+                    dp[i][w]=dp[i-1][w]
+        return dp[n][W]
                 
-        return DP[N][W] 
+        
+                
 
 
        
