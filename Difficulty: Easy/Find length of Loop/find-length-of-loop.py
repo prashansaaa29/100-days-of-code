5db6cccb219a41cf09fd30ecf39bd11a66dc8345
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-08-29 23:16:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find length of Loop/find-length-of-loop.py
@@ -0,0 +1,84 @@
+#User function Template for python3
+
+'''
+Structure of node
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+'''
+class Solution:
+    # Function to find the length of a loop in the linked list.
+    def countNodesInLoop(self, head):
+        first=head
+        second=head
+        while(first!=None and first.next!=None):
+            second=second.next
+            first=first.next.next
+            
+            if(first==second):
+                size=1
+                curr=second.next
+                while(curr!=second):
+                    size+=1
+                    curr=curr.next
+                return size
+        return 0
+        #Your code here
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def print_list(node):
+    while node:
+        print(node.data, end=' ')
+        node = node.next
+    print()
+
+
+def loop_here(head, pos):
+    if pos == 0:
+        return
+
+    walk = head
+    for _ in range(1, pos):
+        walk = walk.next
+
+    tail = head
+    while tail.next:
+        tail = tail.next
+
+    tail.next = walk
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split('\n')
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        arr = list(map(int, data[index].split()))
+        k = int(data[index + 1])
+        head = Node(arr[0])
+        tail = head
+        for value in arr[1:]:
+            tail.next = Node(value)
+            tail = tail.next
+        loop_here(head, k)
+        ob = Solution()
+        res = ob.countNodesInLoop(head)
+        print(res)
+        index += 2
+
+# } Driver Code Ends
