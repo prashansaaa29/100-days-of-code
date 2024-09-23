53ae122671994a9bdee095d2fc883f3c93827fb7
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-09-23 14:58:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing And Repeating/missing-and-repeating.py
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.py
@@ -1,16 +1,18 @@
 #User function Template for python3
 
 class Solution:
-    def findTwoElement( self,A, n):
-        h=set(A)
-        j=list(h)
-        l=(sum(A)-sum(j))
-        A.remove(l)
-        c=n
-        b=(((c*(c+1))//2)-sum(j))
-        k=[l,b]
-        return k
+    def findTwoElement( self,arr):
+        n = len(arr)   # length of arr
+        sum_exp = int(n * (n + 1) / 2)   # expected sum
+        sum_arr = sum(arr)   # actual sum
         
+        set_arr = set(arr)   # unqique elements in arr
+        sum_set = sum(set_arr)   # sum of the unique elements
+        
+        repeating = sum_arr - sum_set
+        missing = sum_exp - sum_set
+        
+        return([repeating, missing])
         # code here
 
 
@@ -18,14 +20,15 @@ class Solution:
  # Driver Code Starts
 #Initial Template for Python 3
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
 
-    tc=int(input())
+    tc = int(input())
     while tc > 0:
-        n=int(input())
-        arr=list(map(int, input().strip().split()))
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
         ob = Solution()
-        ans=ob.findTwoElement(arr, n)
-        print(str(ans[0])+" "+str(ans[1]))
-        tc=tc-1
+        ans = ob.findTwoElement(arr)
+        print(str(ans[0]) + " " + str(ans[1]))
+        tc = tc - 1
+
 # } Driver Code Ends
