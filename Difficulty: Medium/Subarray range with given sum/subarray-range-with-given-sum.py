5edeca9ca59fd1fc96ab2dc9d5a5c59f8b9e744b
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-10-15 03:01:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarray range with given sum/subarray-range-with-given-sum.py
@@ -0,0 +1,41 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    #Complete this fuction
+    #Function to count the number of subarrays which adds to the given sum.
+    def subArraySum(self,arr, tar):
+        #Your code here
+        from collections import defaultdict
+        cnt = defaultdict(int, {0: 1})
+        
+        s, ans = 0, 0
+        for e in arr:
+            s += e
+            ans += cnt[s-tar]
+            cnt[s] += 1
+        return ans
+        #Your code here
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        tar= int(input())
+        ob = Solution()
+        res = ob.subArraySum(arr,tar)
+        print(res)
+        # print("~")
+        t -= 1
+
+
+# } Driver Code Ends
