d90c0680e7a29e6b29172c2460173831666d2f06
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-11-05 00:09:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+def rotate(matrix): 
+    n = len(matrix)
+    
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Step 2: Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
+    
+    return matrix
+
+    #code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        matrix = []
+        for i in range(N):
+            arr = [int(x) for x in input().strip().split()]
+            matrix.append(arr)
+
+        rotate(matrix)
+        for i in range(N):
+            for j in range(N):
+                print(matrix[i][j], end=' ')
+            print()
+        print("~")
+
+# } Driver Code Ends
