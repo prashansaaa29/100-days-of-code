ea7f53fd03cad21968bb0372b7529d34c3ede516
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-11-20 03:06:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element II/majority-element-ii.py
@@ -0,0 +1,41 @@
+class Solution:
+    # Function to find the majority elements in the array
+    def findMajority(self, arr):
+        #Your Code goes here.
+        dic = {}
+        n = len(arr)
+        
+        # Counting occurrences of each element
+        for i in arr:
+            dic[i] = dic.get(i, 0) + 1
+        
+        # Finding elements that appear more than n // 3 times
+        ans = [i for i in dic if dic[i] > (n // 3)]
+        
+        ans.sort()  # Sorting the result
+        return ans
+        #Your Code goes here.
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+    t = int(input().strip())
+    for _ in range(t):
+        s = input().strip()
+        nums = list(map(int, s.split()))
+        ob = Solution()
+        ans = ob.findMajority(nums)
+        if not ans:
+            print("[]")
+        else:
+            print(" ".join(map(str, ans)))
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
