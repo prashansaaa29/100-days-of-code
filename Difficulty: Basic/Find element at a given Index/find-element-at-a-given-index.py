3929bc991a5c94180f43205ee689ca1ac30abdb9
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-12-14 23:42:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find element at a given Index/find-element-at-a-given-index.py
@@ -0,0 +1,27 @@
+
+from typing import List
+
+
+class Solution:
+    def findElementAtIndex(self, key : int, arr : List[int]) -> int:
+        return arr[key]
+        # code here
+        
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        key = int(input())
+        arr = list(map(int, input().strip().split()))
+
+        obj = Solution()
+        res = obj.findElementAtIndex(key, arr)
+
+        print(res)
+        print("~")
+
+# } Driver Code Ends
